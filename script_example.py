6d14f745b09a51146f97ebcc3545e2340e21c3e3
Allen <dukegzy@gmail.com>
2026-01-06 23:42:01 -0500

    gamma_1 2 and initial script

---
+++ script_example.py
@@ -0,0 +1,322 @@
+import numpy as np
+import yaml
+import torch
+from tqdm import tqdm
+from torch.utils.data import DataLoader, random_split
+from torchvision.datasets import MNIST
+from torchvision import transforms
+import mlflow
+from mlflow.types import Schema, TensorSpec
+from mlflow.models import ModelSignature
+
+from sd_vae.ae import VAE
+from trainers import EarlyStopping
+from trainers.first_stage_trainer import CLEAR_VAEFirstStageTrainer
+
+from modules.loss import SupCon, SNN, DenseSupCon, DenseSNN
+
+import data_utils.styled_mnist.corruptions as corruptions
+from data_utils.styled_mnist.data_utils import StyledMNISTGenerator, StyledMNIST
+
+
+
+import torch.nn as nn
+import torch.nn.init as init
+
+
+import matplotlib.pyplot as plt
+from torchvision.utils import make_grid
+from exp_utils.visual import feature_swapping_plot
+from datetime import datetime
+import os
+import math
+from sklearn.manifold import TSNE
+
+
+
+def init_weights(m):
+    if isinstance(m, (nn.Conv2d, nn.ConvTranspose2d)):
+        init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
+        if m.bias is not None:
+            init.zeros_(m.bias)
+
+    elif isinstance(m, nn.Linear):
+        init.xavier_uniform_(m.weight)
+        if m.bias is not None:
+            init.zeros_(m.bias)
+
+    elif isinstance(m, (nn.GroupNorm, nn.BatchNorm2d, nn.LayerNorm)):
+        if m.weight is not None:
+            init.ones_(m.weight)
+        if m.bias is not None:
+            init.zeros_(m.bias)
+
+
+def silu_xavier_init(m):
+    SILU_GAIN = 1.4884  # √2 * √(π/8)
+    
+    if isinstance(m, nn.Linear):
+        nn.init.xavier_uniform_(m.weight, gain=SILU_GAIN)
+        if m.bias is not None:
+            nn.init.zeros_(m.bias)
+    
+    elif isinstance(m, (nn.Conv2d, nn.ConvTranspose2d)):
+        nn.init.xavier_uniform_(m.weight, gain=SILU_GAIN)
+        if m.bias is not None:
+            nn.init.zeros_(m.bias)
+    
+    elif isinstance(m, nn.BatchNorm2d):
+        nn.init.ones_(m.weight)
+        nn.init.zeros_(m.bias)
+
+
+
+
+def save_mu_channels(mu, start, end, title_prefix, filename, nrow_grid=16, out_dir="outputs/images"):
+    """
+    mu: Tensor (B, C, H, W)
+    start/end: channel slice [start, end)
+    """
+    os.makedirs(out_dir, exist_ok=True)
+
+    C = end - start
+    n_cols = int(math.ceil(math.sqrt(C)))
+    n_rows = int(math.ceil(C / n_cols))
+
+    fig, axes = plt.subplots(n_rows, n_cols, figsize=(4 * n_cols, 4 * n_rows))
+    axes = axes.flatten() if C > 1 else [axes]
+
+    for k, i in enumerate(range(start, end)):
+        grid = make_grid(
+            mu[:, i][:, None, :, :],   # (B,1,H,W)
+            nrow=nrow_grid,
+            normalize=True
+        ).cpu().permute(1, 2, 0)
+
+        axes[k].imshow(grid)
+        axes[k].set_title(f"{title_prefix}[{i}]")
+        axes[k].axis("off")
+
+    for j in range(C, len(axes)):
+        axes[j].axis("off")
+
+    plt.tight_layout()
+    path = os.path.join(out_dir, filename)
+    plt.savefig(path, dpi=200, bbox_inches="tight")
+    plt.close()
+    print(f"Saved: {path}")
+
+
+
+def main():
+    with open('config.yaml', 'r') as f:
+        config = yaml.safe_load(f)
+
+    dataset = config['data']['name']
+    # to do: add ways to analyze data
+    train, test, valid = random_split(dataset, [40000, 10000, 10000])
+    train_loader = DataLoader(train, batch_size=128, shuffle=True)
+    valid_loader = DataLoader(valid, batch_size=256, shuffle=False)
+    test_loader = DataLoader(test, batch_size=128, shuffle=False)
+
+    params = config['hyperparameters']
+    vae_config = config['vae_architecture']
+    contrastive_config = config['contrastive_module']
+    device = torch.device(config['device'])
+
+    input_schema = Schema([TensorSpec(np.dtype(np.float32), 
+                                    [-1] + config['data']['input_shape'])])
+    output_schema = Schema([TensorSpec(np.dtype(np.float32), 
+                                    [-1] + config['data']['output_shape'])])
+    signature = ModelSignature(inputs=input_schema, outputs=output_schema)
+
+    vae = VAE(
+        channels=vae_config['channels'],
+        channel_multipliers=vae_config['channel_multipliers'],
+        n_resnet_blocks=vae_config['n_resnet_blocks'],
+        x_channels=vae_config['x_channels'],
+        z_channels=vae_config['z_channels'],
+        norm_channels=vae_config['norm_channels'],
+        n_heads=vae_config['n_heads'],
+    ).to(device)
+
+    if config['trainer']['weight_init'] == 'silu_xavier_init':
+        vae.apply(silu_xavier_init)
+
+    trainer = CLEAR_VAEFirstStageTrainer(
+        model=vae,
+        early_stopping=EarlyStopping(
+            patience=config['trainer']['early_stopping']['patience']
+        ),
+        verbose_period=config['trainer']['verbose_period'],
+        device=device,
+        model_signature=signature,
+        args={
+            "beta": params['beta'],
+            "gamma_1": params['gamma_1'],
+            "gamma_2": params['gamma_2'],
+            "vae_lr": params['lr'],
+            "channel_split": vae_config['channel_split'],
+            "contrastive_module": contrastive_config,
+        },
+    )
+
+
+    mlflow.set_tracking_uri("./mlruns")
+    mlflow.set_experiment("test")
+    with mlflow.start_run():
+        mlflow.log_params(params)
+        trainer.fit(epochs=config['epochs'], train_loader=train_loader, valid_loader=valid_loader)
+
+    print("content tau:", trainer.contrastive_criterions['global']['content'].log_tau.exp().item())
+    print("style tau:", trainer.contrastive_criterions['global']['style'].log_tau.exp().item())
+    x = next(iter(test_loader))['image'].to(device)
+    best_model = trainer.model
+    
+    with torch.no_grad():
+        best_model.eval()
+        xhat, posterior = best_model(x)
+
+    
+
+
+    mu = posterior.mu
+    content_dim, style_dim = config["vae_architecture"]["channel_split"]  # [3, 12]
+    C_total = mu.shape[1]
+    assert content_dim + style_dim == C_total, f"split sum {content_dim+style_dim} != mu channels {C_total}"
+
+    timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+
+    # content: [0, content_dim)
+    save_mu_channels(
+        mu,
+        start=0,
+        end=content_dim,
+        title_prefix="content_mu",
+        filename=f"{timestamp}_content_mu.png",
+        out_dir= config['image_path']
+    )
+
+    # style: [content_dim, content_dim+style_dim)
+    save_mu_channels(
+        mu,
+        start=content_dim,
+        end=content_dim + style_dim,
+        title_prefix="style_mu",
+        filename=f"{timestamp}_style_mu.png",
+        out_dir= config['image_path']
+    )
+    z_c, z_s = mu.split_with_sizes(params['channel_split'], dim=1)
+    x = next(iter(test_loader))['image'].to(device)
+
+    select = torch.randint(0, 128, (16,)).tolist()
+    feature_swapping_plot(
+        z_c[select],
+        z_s[select],
+        x[select],
+        best_model,
+        save=True,
+        name="swap_content_style",
+        out_dir= config['image_path']
+    )
+
+    z_cs = []
+    z_ss = []
+    labels = []
+    styles = []
+
+    with torch.no_grad():
+        best_model.eval()
+        for batch in tqdm(test_loader):
+            x = batch['image'].to(device)
+            _, posterior = best_model(x)
+            z_c, z_s = posterior.sample().split_with_sizes(vae_config['channel_split'], dim=1)
+            z_cs.append(z_c.cpu())
+            z_ss.append(z_s.cpu())
+            labels.append(batch['label'])
+            styles.append(batch['style'])
+
+    z_cs = torch.cat(z_cs, dim=0)
+    z_ss = torch.cat(z_ss, dim=0)
+    labels = torch.cat(labels, dim=0)
+    styles = torch.cat(styles, dim=0)
+
+    tsne = TSNE(n_components=2, init='pca')
+    z_2d = tsne.fit_transform(z_cs.view(z_cs.shape[0], -1).numpy())
+
+    fig, axs = plt.subplots(1, 2, figsize=(10, 4))
+
+    axs0 = axs[0].scatter(z_2d[:, 0], z_2d[:, 1], c=labels.numpy(), cmap='tab10', alpha=0.2)
+    cbar = fig.colorbar(axs0, ax=axs[0])
+    axs[0].set_title('color by content')
+
+    style_labels = config['TSNE']['style_label']
+    cmap = plt.get_cmap('Set1')
+    colors = [cmap(i) for i in np.linspace(0, 1, len(style_labels))]
+    for g in range(len(style_labels)):
+        i = np.where(styles == g)[0]
+        axs[1].scatter(z_2d[i,0], z_2d[i,1], alpha=0.2, c=colors[g], label=style_labels[g])
+    axs[1].legend()
+    plt.savefig(os.path.join(config['image_path'], "tsne_plot_1.png"), dpi=200, bbox_inches="tight")
+
+
+    X = z_ss.view(z_ss.shape[0], -1).cpu().numpy()
+    y_content = labels.cpu().numpy()
+    y_style = styles.cpu().numpy()
+
+    N = X.shape[0]
+    idx = np.random.choice(N, size=N, replace=False)
+
+    X_sub = X[idx]
+    content_sub = y_content[idx]
+    style_sub = y_style[idx]
+
+    tsne = TSNE(
+        n_components=2,
+        init='pca',
+        perplexity=100,
+        learning_rate=800,
+        max_iter=8000,
+        early_exaggeration=40,
+        random_state=0,
+    )
+    z_2d = tsne.fit_transform(X_sub)   
+
+    fig, axs = plt.subplots(1, 2, figsize=(10, 4))
+
+    axs0 = axs[0].scatter(
+        z_2d[:, 0],
+        z_2d[:, 1],
+        c=content_sub,        
+        cmap='tab10',
+        alpha=0.4,
+        s=5,
+    )
+    cbar = fig.colorbar(axs0, ax=axs[0])
+    axs[0].set_title('color by content (5 types)')
+
+    style_labels = ['red', 'green', 'blue', 'yellow', 'purple']
+    cmap = plt.get_cmap('Set1')
+    colors = [cmap(i) for i in np.linspace(0, 1, len(style_labels))]
+
+    for g in range(len(style_labels)):
+        i = np.where(style_sub == g)[0]   
+        axs[1].scatter(
+            z_2d[i, 0],
+            z_2d[i, 1],
+            alpha=0.6,
+            c=[colors[g]],             
+            s=5,
+            label=style_labels[g],
+        )
+
+    axs[1].set_title('color by style (5 types)')
+    axs[1].legend()
+    plt.tight_layout()
+    plt.savefig(os.path.join(config['image_path'], "tsne_plot_2.png"), dpi=200, bbox_inches="tight")
+
+
+
+
+if __name__ == "__main__":
+    main()
---
+++ trainers/first_stage_trainer.py
@@ -0,0 +1,388 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import mlflow
+
+from torch.utils.data import DataLoader
+from tqdm import tqdm
+from mlflow.models import ModelSignature
+
+from sd_vae.ae import VAE
+from modules.loss import d_hinge_loss, g_hinge_loss
+from modules.loss import SupCon, SNN, DenseSupCon, DenseSNN  # noqa
+from . import EarlyStopping, Trainer
+
+
+class VAEFirstStageTrainer(Trainer):
+    def __init__(
+        self,
+        model: VAE,
+        discriminator: nn.Module | None,
+        early_stopping: EarlyStopping | None,
+        verbose_period: int,
+        device: torch.device,
+        model_signature: ModelSignature,
+        args: dict,
+        transform=None,
+    ) -> None:
+        super().__init__(
+            model,
+            early_stopping,
+            verbose_period,
+            device,
+            model_signature,
+            args,
+            transform,
+        )
+        # TODO: add lpips
+        self.discriminator = discriminator.to(device) if discriminator else None
+        if self.discriminator:
+            self.discriminator.apply(self.discriminator.weight_init)
+            self.logvar = nn.Parameter(torch.zeros(size=()))
+        self.opts = self._configure_opts(args)
+
+    def _configure_opts(self, args: dict):
+        if self.discriminator is None:
+            vae_opt = optim.Adam(self.model.parameters(), lr=args["vae_lr"])
+            return {"vae_opt": vae_opt}
+        else:
+            vae_opt = optim.Adam(
+                list(self.model.parameters()) + [self.logvar], lr=args["vae_lr"]
+            )
+            disc_opt = optim.Adam(self.discriminator.parameters(), lr=args["disc_lr"])
+            return (
+                {"vae_opt": vae_opt, "disc_opt": disc_opt}
+                if self.discriminator
+                else {"vae_opt": vae_opt}
+            )
+
+    def disc_factor(self, cur_step: int):
+        return 1 if cur_step >= self.args["disc_warmup"] else 0
+
+    def adaptive_d_weight(self, rec_loss, g_loss):
+        last_layer = self.model.decoder.conv_out.weight
+        nll_loss = (
+            rec_loss / self.logvar.exp() + self.logvar
+        )  # update to factor of 2 and additive constant
+        nll_grad = torch.autograd.grad(nll_loss, last_layer, retain_graph=True)[0]
+        g_grad = torch.autograd.grad(g_loss, last_layer, retain_graph=True)[0]
+        d_weight = torch.norm(nll_grad) / (torch.norm(g_grad) + 1e-8)
+        d_weight = torch.clamp(d_weight, 0.0, 1e4).detach()
+        return d_weight
+
+    def _train(self, dataloader: DataLoader, verbose: bool, epoch_id: int):
+        has_disc = self.discriminator is not None
+
+        vae: VAE = self.model
+        vae.train()
+        vae_opt = self.opts["vae_opt"]
+        if has_disc:
+            disc_opt = self.opts["disc_opt"]
+        device = self.device
+
+        beta = self.args["beta"]
+
+        with tqdm(dataloader, unit="batch", mininterval=0, disable=not verbose) as bar:
+            bar.set_description(f"Epoch {epoch_id}")
+
+            for batch_id, batch in enumerate(bar):
+                cur_step = epoch_id * len(dataloader) + batch_id
+                x = batch["image"].to(device)
+                if self.transform:
+                    x = self.transform(x)
+                xhat, posterior = vae(x)
+
+                rec_loss = (
+                    F.mse_loss(xhat, x, reduction="none").sum(dim=(1, 2, 3)).mean()
+                )
+                kl_loss = posterior.kl().mean()
+                loss = rec_loss + beta * kl_loss
+                metrics = {
+                    "rec_loss": rec_loss.item(),
+                    "kl_loss": kl_loss.item(),
+                }
+                if has_disc:
+                    disc_factor = self.disc_factor(cur_step)
+                    g_loss = g_hinge_loss(fake_logits=self.discriminator(xhat))
+
+                    d_weight = self.adaptive_d_weight(rec_loss, g_loss)
+                    loss = loss + disc_factor * d_weight * g_loss
+                    metrics.update(
+                        {"g_loss": g_loss.item(), "d_weight": d_weight.item()}
+                    )
+
+                vae_opt.zero_grad()
+                loss.backward()
+                vae_opt.step()
+
+                if has_disc:
+                    d_loss = disc_factor * d_hinge_loss(
+                        real_logits=self.discriminator(x),
+                        fake_logits=self.discriminator(xhat.detach()),
+                    )  # no gradient path from D → G.
+                    metrics.update({"d_loss": d_loss.item()})
+                    disc_opt.zero_grad()
+                    d_loss.backward()
+                    disc_opt.step()
+
+                # update progress bar
+                bar.set_postfix(metrics)
+                if cur_step % 10 == 0:
+                    mlflow.log_metrics(metrics, step=cur_step)
+        return
+
+    def evaluate(self, dataloader: DataLoader, verbose: bool):
+        vae: VAE = self.model
+        vae.eval()
+        device = self.device
+
+        total_rec_loss, total_kl_loss = 0.0, 0.0
+        with torch.no_grad():
+            for batch in tqdm(
+                dataloader, unit="batch", mininterval=0, disable=not verbose
+            ):
+                x = batch["image"].to(device)
+                if self.transform:
+                    x = self.transform(x)
+                xhat, posterior = vae(x)
+                rec_loss = (
+                    F.mse_loss(xhat, x, reduction="none").sum(dim=(1, 2, 3)).mean()
+                )
+                kl_loss = posterior.kl()
+
+                total_rec_loss += rec_loss.mean().item()
+                total_kl_loss += kl_loss.mean().item()
+
+        val_mse = total_rec_loss / len(dataloader)
+        val_kl = total_kl_loss / len(dataloader)
+        return val_mse, val_kl
+
+    def _valid(self, dataloader: DataLoader, verbose: bool, epoch_id: int):
+        val_mse, val_kl = self.evaluate(dataloader, verbose)
+        if verbose:
+            print(f"epoch {epoch_id}/val_mse: {val_mse:.4f}")
+        valid_metrics = {
+            "callback_metric": val_mse,
+            "logged_metric": {
+                "val_mse": val_mse,
+                "val_kl": val_kl,
+            },
+        }
+        return valid_metrics
+
+
+class CLEAR_VAEFirstStageTrainer(Trainer):
+    def __init__(
+        self,
+        model: nn.Module,
+        early_stopping: EarlyStopping | None,
+        verbose_period: int,
+        device: torch.device,
+        model_signature: ModelSignature,
+        args: dict,
+        transform=None,
+    ) -> None:
+        super().__init__(
+            model,
+            early_stopping,
+            verbose_period,
+            device,
+            model_signature,
+            args,
+            transform,
+        )
+        self._configure_contrastive_criterion(args)
+        self._configure_opts(args)
+        self.args = args
+
+    def _configure_contrastive_criterion(self, args: dict):
+        args = args["contrastive_module"]
+        contrastive_criterions = {
+            "global": {
+                "content": eval(f"{args['contrastive_method']}")(
+                    temperature=args["temperature"][0],
+                    learnable_temp=args["learnable_temp"],
+                    pool=args["pool"],
+                    use_proj=args["use_proj"],
+                ).to(self.device),
+                "style": eval(f"{args['contrastive_method']}")(
+                    temperature=args["temperature"][1],
+                    learnable_temp=args["learnable_temp"],
+                    pool=args["pool"],
+                    use_proj=args["use_proj"],
+                ).to(self.device),
+            }
+        }
+        if args.get("use_dense"):
+            contrastive_criterions["dense"] = {
+                # "content": eval(f"Dense{args['contrastive_method']}")(
+                "content": DenseSupCon(
+                    args["temperature"][0],
+                    args["learnable_temp"],
+                ).to(self.device),
+                "style": DenseSupCon(
+                    args["temperature"][1],
+                    args["learnable_temp"],
+                ).to(self.device),
+            }
+        self.contrastive_criterions = contrastive_criterions
+
+    def _configure_opts(self, args: dict):
+        param_list = (
+            list(self.model.parameters())
+            + list(self.contrastive_criterions["global"]["content"].parameters())
+            + list(self.contrastive_criterions["global"]["style"].parameters())
+        )
+        if "dense" in self.contrastive_criterions:
+            param_list += list(
+                self.contrastive_criterions["dense"]["content"].parameters()
+            ) + list(self.contrastive_criterions["dense"]["style"].parameters())
+
+        vae_opt = optim.Adam(param_list, lr=args["vae_lr"])
+        self.opts = {"vae_opt": vae_opt}
+
+    def _train(self, dataloader: DataLoader, verbose: bool, epoch_id: int):
+        vae: VAE = self.model
+        vae.train()
+        opt = self.opts["vae_opt"]
+        device = self.device
+
+        channel_split = self.args["channel_split"]
+        beta = self.args["beta"]
+        gamma_1 = self.args["gamma_1"]
+        gamma_2 = self.args["gamma_2"]
+
+        with tqdm(dataloader, unit="batch", mininterval=0, disable=not verbose) as bar:
+            bar.set_description(f"Epoch {epoch_id}")
+            for batch_id, batch in enumerate(bar):
+                x, y = batch["image"].to(device), batch["label"].to(device)
+                if self.transform:
+                    x = self.transform(x)
+                opt.zero_grad()
+                xhat, posterior = vae(x)
+                z_c, z_s = posterior.sample().split_with_sizes(channel_split, dim=1)
+
+                rec_loss = (
+                    F.mse_loss(xhat, x, reduction="none").sum(dim=(1, 2, 3)).mean()
+                )
+                kl_loss = posterior.kl().mean()
+                con_loss = self.contrastive_criterions["global"]["content"](
+                    z_c, y
+                ).mean()
+                ps_loss = self.contrastive_criterions["global"]["style"](
+                    z_s, y, ps=True
+                ).mean()
+
+                if "dense" in self.contrastive_criterions:
+                    dense_con_loss = self.contrastive_criterions["dense"]["content"](
+                        z_c, y
+                    ).mean()
+                    dense_ps_loss = self.contrastive_criterions["dense"]["style"](
+                        z_s, y, ps=True
+                    ).mean()
+                    loss = (
+                        rec_loss
+                        + beta * kl_loss
+                        + gamma_1 * (0.5 * con_loss + 0.5 * dense_con_loss)
+                        + gamma_2 * (0.5 * ps_loss + 0.5 * dense_ps_loss)
+                    )
+                else:
+                    dense_con_loss = torch.tensor(0.0, device=device)
+                    dense_ps_loss = torch.tensor(0.0, device=device)
+                    loss = rec_loss + beta * kl_loss + gamma_1 * con_loss + gamma_2 * ps_loss
+
+                loss.backward()
+                opt.step()
+
+                bar.set_postfix(
+                    rec_loss=rec_loss.item(),
+                    kl_loss=kl_loss.item(),
+                    con_loss=con_loss.item(),
+                    ps_loss=ps_loss.item(),
+                    dense_con_loss=dense_con_loss.item(),
+                    dense_ps_loss=dense_ps_loss.item(),
+                )
+
+                cur_step = epoch_id * len(dataloader) + batch_id
+                if cur_step % 50 == 0:
+                    mlflow.log_metrics(
+                        {
+                            "rec_loss": rec_loss.item(),
+                            "kl_loss": kl_loss.item(),
+                            "con_loss": con_loss.item(),
+                            "ps_loss": ps_loss.item(),
+                            "dense_con_loss": dense_con_loss.item(),
+                            "dense_ps_loss": dense_ps_loss.item(),
+                        },
+                        step=cur_step,
+                    )
+        return
+
+    def evaluate(self, dataloader: DataLoader, verbose: bool):
+        vae: VAE = self.model
+        vae.eval()
+        device = self.device
+        channel_split = self.args["channel_split"]
+
+        losses = {
+            "rec_loss": 0.0,
+            "kl_loss": 0.0,
+            "con_loss": 0.0,
+            "ps_loss": 0.0,
+            "dense_con_loss": 0.0,
+            "dense_ps_loss": 0.0,
+        }
+        with torch.no_grad():
+            for batch in tqdm(
+                dataloader, unit="batch", mininterval=0, disable=not verbose
+            ):
+                x, y = batch["image"].to(device), batch["label"].to(device)
+                if self.transform:
+                    x = self.transform(x)
+                xhat, posterior = vae(x)
+                z_c, z_s = posterior.sample().split_with_sizes(channel_split, dim=1)
+
+                rec_loss = (
+                    F.mse_loss(xhat, x, reduction="none").sum(dim=(1, 2, 3)).mean()
+                )
+                kl_loss = posterior.kl().mean()
+                con_loss = self.contrastive_criterions["global"]["content"](
+                    z_c, y
+                ).mean()
+                ps_loss = self.contrastive_criterions["global"]["style"](
+                    z_s, y, ps=True
+                ).mean()
+
+                losses["rec_loss"] += rec_loss.item()
+                losses["kl_loss"] += kl_loss.item()
+                losses["con_loss"] += con_loss.item()
+                losses["ps_loss"] += ps_loss.item()
+
+                if "dense" in self.contrastive_criterions:
+                    dense_con_loss = self.contrastive_criterions["dense"]["content"](
+                        z_c, y
+                    ).mean()
+                    dense_ps_loss = self.contrastive_criterions["dense"]["style"](
+                        z_s, y, ps=True
+                    ).mean()
+                    losses["dense_con_loss"] += dense_con_loss.item()
+                    losses["dense_ps_loss"] += dense_ps_loss.item()
+
+        for k in losses:
+            losses[k] /= len(dataloader)
+
+        return losses
+
+    def _valid(self, dataloader: DataLoader, verbose: bool, epoch_id: int):
+        val_losses = self.evaluate(dataloader, verbose)
+        val_rec = val_losses["rec_loss"]
+        if verbose:
+            print(f"epoch {epoch_id}/val_rec: {val_rec:.4f}")
+
+        logged_metric = {f"val_{k}": v for k, v in val_losses.items()}
+        valid_metrics = {
+            "callback_metric": val_rec,
+            "logged_metric": logged_metric,
+        }
+        return valid_metrics
